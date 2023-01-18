9e3b5dfcc4563f5a8e3e2f61ae809340f75847d5
Michael7543 <davidmichael7543@hotmail.com>
2023-01-18 17:46:31 -0500

    18/01/2023 -Relacion entre la entidad category y product -Mapeo de columnas

---
+++ server/src/modules-sv/entities/product.model.ts
@@ -0,0 +1,64 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CategoryEntity } from "./category.model";
+
+@Entity('products',{schema:'ventasbdd'})
+
+export class ProductEntity{
+    //columnas de la entidad 
+    @PrimaryGeneratedColumn('identity')
+    id:string;
+    @CreateDateColumn({
+        //objeto
+        name:'create_at',
+        type:'timestamptz',
+        default:() => 'CURRENT_TIMESTAMP',
+    })
+    
+    createAT:Date;//atributo
+
+    @UpdateDateColumn({
+        name:'create_at',
+        type:'timestamptz',
+        default:() => 'CURRENT_TIMESTAMP',
+    })
+    updateAt:Date;
+
+    @DeleteDateColumn({
+        name:'create_at',
+        type:'timestamptz',
+        nullable:true,
+    })
+
+    deleteAt:Date; //el nombre del atributo
+    //Relationships
+    //paramtrro de entrada =>
+    @ManyToOne(() => CategoryEntity, (category) => category.product)
+    @JoinColumn({name:'product_id'})
+    category: CategoryEntity;
+
+    @Column('varchar',{
+        name:'title',
+        unique:true,
+        comment: 'nombre del producto',
+    })
+    title:string;
+
+    @Column('number',{
+        name:'price',//nombre de la tabla
+        comment:'precio del  producto',
+    })
+    
+    price:number;//nombre del atributo
+    
+    @Column('varchar',{
+        name:'description',
+        comment:'descripcion del producto'
+    })
+    description:string;
+}
+
+
+
+
+
+//user es categoria y photo es producto
